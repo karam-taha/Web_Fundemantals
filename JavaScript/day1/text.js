9fde0d6fc944113057582735e9cae42b2a80aaad
Karam Taha <karamtaha15@gmail.com>
2022-08-29 13:25:42 +0300

    class code testing

---
+++ JavaScript/day1/text.js
@@ -0,0 +1,20 @@
+var universityName = "Najah";
+var major = "civil engineering";
+
+if(universityName == "Najah" && major=="civil engineering"){
+        console.log("Studied Civil Engineering in Najah");
+    }
+    
+    else{
+        console.log("did not Study Civil Engineering in Najah");
+    }
+
+
+    console.log("Wadee"+5+5);
+    console.log(5+5+"Wadee");
+
+var text="*";
+for(i=0; i<20; i++){
+    console.log(text);
+    text+="*";
+}
