4378611429f0ea384a73390a987b4e292ac33e5d
Karam Taha <karamtaha15@gmail.com>
2022-08-29 22:35:18 +0300

    asignment in js

---
+++ JavaScript/video-preview/script.js
@@ -0,0 +1,13 @@
+
+
+var x = document.getElementById("myVideo");
+
+function playVid() {
+    x.play();
+    }
+    
+    function pauseVid() {
+    x.pause();
+    }
+    
+// console.log("myVideo");
