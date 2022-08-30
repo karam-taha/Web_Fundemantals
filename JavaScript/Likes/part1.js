f6b1d856b6c47e6f37fa6d968125efe892afd2a0
Karam Taha <karamtaha15@gmail.com>
2022-08-30 21:18:02 +0300

    Likes asignment

---
+++ JavaScript/Likes/part1.js
@@ -0,0 +1,4 @@
+function addLike(){
+    var likesbtn = document.getElementById("likecount").textContent;
+    document.getElementById("likecount").textContent= ++likesbtn;
+}
---
+++ JavaScript/Likes/part2.js
@@ -0,0 +1,15 @@
+
+//writ documention for it, what it will do??
+function addLike(id){
+    var likesbtn = document.getElementById(id).textContent;
+    document.getElementById(id).textContent= ++likesbtn;
+}
+//if we want to use multiple buttons we use id and go to the [onclick="addLike('likecount2')"] and type likecount2 inside it instead of empty
+
+
+// function addLike(){
+//     var likesbtn = document.getElementById("likecount").textContent;
+//     document.getElementById("likecount").textContent= ++likesbtn;
+// }
+
+//    if we want to use one button only
