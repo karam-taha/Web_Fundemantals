5a8675b900cdea207cc3e5d1d2550d3d4291e256
Karam Taha <karamtaha15@gmail.com>
2022-08-31 16:46:07 +0300

    DojoWeather asignment

---
+++ JavaScript/DojoWeather/DojoWeather.js
@@ -0,0 +1,8 @@
+function removeElement(element1){   //or removeElement=()=>{
+    var element=element1.parentElement;
+        element.remove();
+    }
+
+    function clickAlert(){
+        alert("Loading weather report...")
+    }
