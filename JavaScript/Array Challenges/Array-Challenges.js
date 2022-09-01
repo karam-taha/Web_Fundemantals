78c5a08290d9fd0738b24bdd7755374ef85d25d4
Karam Taha <karamtaha15@gmail.com>
2022-09-01 15:30:41 +0300

    array challenges asignment

---
+++ JavaScript/Array Challenges/Array-Challenges.js
@@ -0,0 +1,86 @@
+//1. Always hungry
+
+function alwaysHungry(arr) {
+    var counter = 0;
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]=="food"){
+            console.log("yummy");
+            counter++;
+        }
+    }
+    if(counter==0){
+        console.log("I'm hungry");
+    }
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+
+alwaysHungry([4, 1, 5, 7, 2]);
+
+
+//2. High Pass Filter
+
+function highPass(arr, cutoff) {
+    var filteredArr = []; x=0;
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]>cutoff){
+            filteredArr[x] = arr[i];
+            x++
+        }
+        
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+
+//3. Better than average
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    for(var i=0; i<arr.length; i++){
+        sum+=arr[i];
+        avg=sum/arr.length;
+    }
+    var count = 0
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]>avg){
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+
+//4. Array Reverse
+
+function reverse(arr) {
+    var arrNew=[];
+    for(var i=arr.length-1; i>=0; i--){
+        arrNew.push(arr[i]);
+        
+    }
+    return arrNew;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+
+
+//5. Fibonacci Array
+
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    for(var i=2; i<n; i++){
+        fibArr[i]=fibArr[i-1]+fibArr[i-2];
+    }
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
