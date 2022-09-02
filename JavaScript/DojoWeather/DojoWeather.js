9ee96df3b089530ed1d8ddff45760ddfd2356faa
Karam Taha <karamtaha15@gmail.com>
2022-09-02 11:03:25 +0300

    updated challenge + minor js changes

--- JavaScript/DojoWeather/DojoWeather.js
+++ JavaScript/DojoWeather/DojoWeather.js
@@ -1,8 +1,38 @@
-function removeElement(element1){   //or removeElement=()=>{
-    var element=element1.parentElement;
+// function removeElement(element1){          //or removeElement=()=>{
+//     var element=element1.parentElement;                               different way to select the parent as a whole
+//         element.remove();
+//     }
+
+    function removeElement(element){
+        var element=document.querySelector(".message")                   // tested using querySelector
         element.remove();
     }
 
     function clickAlert(){
         alert("Loading weather report...")
     }
+
+    function c2f(temp) {
+        return Math.round(9 / 5 * temp + 32) + "°";
+    }
+    
+    function f2c(temp) {
+        return Math.round(5 / 9 * (temp - 32)) + "°";
+        
+    }
+    function changeDegree(element) {
+        console.log(element.value);
+        for(var i=1; i<9; i++) {
+            var tempSpan = document.querySelector("#temp" + i);
+            var tempVal = parseInt(tempSpan.innerText);
+            if(element.value == "°C") {
+                tempSpan.innerText = f2c(tempVal);
+            } else {
+                tempSpan.innerText = c2f(tempVal);
+            }
+        }
+    }
+
+    // function changeDegree(){
+    //     document.querySelectorAll
+    // }
